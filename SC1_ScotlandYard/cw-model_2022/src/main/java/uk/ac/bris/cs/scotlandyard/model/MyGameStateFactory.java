1f298cfd3f0953dd7a61ccb910237788d7170276
AntonioCOM <alcomachado@gmail.com>
2022-03-18 19:39:01 +0000

    GSF class

--- SC1_ScotlandYard/cw-model_2022/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ SC1_ScotlandYard/cw-model_2022/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -11,6 +11,7 @@ import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * cw-model
@@ -54,24 +55,58 @@ public final class MyGameStateFactory implements Factory<GameState>{
 						ImmutableSet.<Piece>builder()
 								.add(GSFmrX.piece())
 								//.addAll( GSFdetectives.iterator() GSFdetectives.piece())
-								//GSFdetectives.forEach( (p) -> (List<Piece> DP =  p.piece()));
+								//GSFdetectives.forEach((p) -> (List<Piece> DP =  p.piece()));
 								.addAll(DP)
 								.build();
-
 				return GSFplayers;
-
 			}
 
 			@Nonnull
 			@Override
 			public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
-				return Optional.empty();
+				/**
+				 * @param detective the detective
+				 * @return the location of the given detective; empty if the detective is not part of the game
+				 */
+				var ref = new Object() {
+					Player id = null;
+				};
+				GSFdetectives.forEach((p) -> {
+					if(p.piece() == detective){ref.id = p;}
+				});
+				return Optional.ofNullable(ref.id.location());
 			}
 
 			@Nonnull
 			@Override
 			public Optional<TicketBoard> getPlayerTickets(Piece piece) {
-				return Optional.empty();
+				/**
+				 * @param piece the player piece
+				 * @return the ticket board of the given player; empty if the player is not part of the game
+				 */
+				var ref = new Object() {
+					Player id = null;
+				};
+
+				if(piece.isDetective()) {
+					GSFdetectives.forEach((p) -> {
+						//id = p.piece();
+						if (p.piece() == piece) {ref.id = p;}
+					});
+				}
+				else {ref.id = mrX;}
+
+				TicketBoard GSFTB = new TicketBoard() {
+					@Override
+					public int getCount(@Nonnull ScotlandYard.Ticket ticket) {
+						/**
+						 * @param ticket the ticket to check count for
+						 * @return the amount of ticket,that player has, always &gt;>= 0
+						 */
+						return ref.id.tickets().get(ticket);
+					}
+				};
+				return Optional.ofNullable(GSFTB);
 			}
 
 			@Nonnull
